a33a838e678a09847e6caaaed0592913e15dea2e
Gaby Hernandez <gabyhernandezlebron@gmail.com>
2017-11-01 14:44:33 -0400

    moves dummy dispatch data to playground file

--- src/app.js
+++ src/app.js
@@ -14,27 +14,7 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense( {description: 'Water Bill', amount: 5000 }));
-store.dispatch(addExpense( {description: 'Gas Bill', amount: 3000 , createdAt: 1000}));
-store.dispatch(addExpense( {description: 'Rent', amount: 109500 }));
-
-
-// store.dispatch(setTextFilter('water'));
-
-// setTimeout(() => {
-// store.dispatch(setTextFilter('bill'));
-// }, 3000)
-
-const state = store.getState(); // getting current state
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-console.log(store.getState());
-
-
-
 // use connect for every single component that needs to connect to the Redux store
-
 
 // React-redux library
 // we are going to be using <Provider store> once at the root of our application
